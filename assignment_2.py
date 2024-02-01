1fa436ad1f754745dff5739d04e806e5e128d58b
Yijiang Xie <yijiang.xie@kaust.edu.sa>
2024-02-01 14:46:08 +0300

    Update assignment_2.py

--- assignment_2.py
+++ assignment_2.py
@@ -3,7 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import pdb
 import xarray as xr
-#opening of each of the CIMP6 netcdf files where dset1 represents 1850-1949, dset2 represents 1950-2014, dset3 represents 2015-2100 ssp119, dset4 represents 2015-2100 ssp245, dset5 represents ssp585
+
 dset1 = xr.open_dataset(r'D:\Geo-Environmental\Climate_Model_Data\tas_Amon_GFDL-ESM4_historical_r1i1p1f1_gr1_185001-194912.nc')
 dset2 = xr.open_dataset(r'D:\Geo-Environmental\Climate_Model_Data\tas_Amon_GFDL-ESM4_historical_r1i1p1f1_gr1_195001-201412.nc')
 dset3 = xr.open_dataset(r'D:\Geo-Environmental\Climate_Model_Data\tas_Amon_GFDL-ESM4_ssp119_r1i1p1f1_gr1_201501-210012.nc')
@@ -11,11 +11,11 @@ dset4 = xr.open_dataset(r'D:\Geo-Environmental\Climate_Model_Data\tas_Amon_GFDL-
 dset5 = xr.open_dataset(r'D:\Geo-Environmental\Climate_Model_Data\tas_Amon_GFDL-ESM4_ssp585_r1i1p1f1_gr1_201501-210012.nc')
 
 
-#mean air temperature map for 1850–1900. This takes data from dset1 (1850-1949)
+#Mean air temperature maps 
 np.mean(dset1['tas'].sel(time=slice('18500101', '19001231')), axis=0)
 gr1=np.mean(dset1['tas'].sel(time=slice('18500101', '19001231')), axis=0)
 
-#mean air temperature map for 2071-2100. For the ssp119, ssp245, ssp585
+#Mean air temperature maps 
 
 np.mean(dset3['tas'].sel(time=slice('20710101', '21001231')), axis=0)
 ssp119=np.mean(dset3['tas'].sel(time=slice('20710101', '21001231')), axis=0)
@@ -26,7 +26,7 @@ ssp245=np.mean(dset4['tas'].sel(time=slice('20710101', '21001231')), axis=0)
 np.mean(dset5['tas'].sel(time=slice('20710101', '21001231')), axis=0)
 ssp585=np.mean(dset5['tas'].sel(time=slice('20710101', '21001231')), axis=0)
 
-#plots of mean air temperature map for 1850-1949 and 2071-2100 including ssp119, ssp245, ssp585
+#Plots of mean air temperature maps
 
 plt.imshow(gr1)
 plt.title('Mean Air Temperature Map 1850-1900')
@@ -58,4 +58,4 @@ pdb.set_trace()
 
 
 
-
+ds = xr.open_dataset(r'D:\Geo-Environmental\Climate_Model_Data\tas_Amon_GFDL-ESM4_historical_r1i1p1f1_gr1_185001-194912.nc')
